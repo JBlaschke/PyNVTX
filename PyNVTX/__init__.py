c3e5a9143722ab272283642bd98d65408e910033
Johannes Blaschke <jpblaschke@lbl.gov>
2020-12-09 11:49:24 -0800

    add more auto-instrumentation code

--- PyNVTX/__init__.py
+++ PyNVTX/__init__.py
@@ -5,9 +5,12 @@ from functools import wraps
 
 from PyNVTX_backend import *
 
+
 major_version   = 0;
 minor_version   = 1;
-release_version = 2;
+release_version = 3;
+
+NVTX_IDENTIFIER = "NVTX"
 
 
 
@@ -21,3 +24,33 @@ def mark(label):
             return ret
         return wrapped
     return _mark
+
+
+
+def is_decorated(cls, identifier):
+    for attr in cls.__dict__:
+        if attr == dec_id_str(identifier):
+            return True
+
+
+
+def dec_id_str(identifier):
+    return f"__deco_{identifier}"
+
+
+
+def mark_all_methods(cls):
+    if is_decorated(cls, NVTX_IDENTIFIER):
+        return
+
+    for attr in cls.__dict__:
+        if callable(getattr(cls, attr)):
+            dec = mark(f"{cls}.{attr}")
+            setattr(cls, attr, dec(getattr(cls, attr)))
+
+    setattr(cls, dec_id_str(NVTX_IDENTIFIER), True)
+
+    for base in cls.__bases__:
+        if base == object:
+           continue
+        mark_all_methods(decorator, base)
--- test/test-nvtx.py
+++ test/test-nvtx.py
@@ -1,75 +1,121 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import sys
 import numpy  as np
 import PyNVTX as nvtx
 from   time   import sleep
 from   atexit import register
 from   mpi4py import MPI
 
+#_______________________________________________________________________________
+# Initialize CUDA Driver
+#
+import pycuda
+import pycuda.driver as drv
+
+drv.init()
+
+from pycuda.gpuarray import GPUArray, to_gpu
+from pycuda.compiler import SourceModule
+
+#-------------------------------------------------------------------------------
 
 
-@nvtx.mark("test_function")
-def test():
-    #___________________________________________________________________________
-    # Initialize CUDA Driver
-    #
-    nvtx.RangePushA("Initializing PyCUDA Driver")
+#_______________________________________________________________________________
+# Configure device context to use the GPU belonging to this rank
+#
+nvtx.RangePushA("Setting Device Context")
 
-    import pycuda
-    import pycuda.driver    as drv
-    from   pycuda.gpuarray import GPUArray, to_gpu
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+register(MPI.Finalize)
 
-    drv.init()
+dev = drv.Device(rank)
+ctx = dev.make_context()
+# ctx.push()  # NOTE: `make_context` seems to already push
+register(ctx.pop)
 
-    nvtx.RangePop()
-    #---------------------------------------------------------------------------
-
-
-    #___________________________________________________________________________
-    # Generate sample data
-    #
-    nvtx.RangePushA("Generating Random Data")
-
-    giant_data = np.random.rand(1024**3)
-
-    nvtx.RangePop()
-    #---------------------------------------------------------------------------
+nvtx.RangePop()
+#-------------------------------------------------------------------------------
 
 
-    #___________________________________________________________________________
-    # Configure device context to use the GPU belonging to this rank
-    #
-    nvtx.RangePushA("Setting Device Context")
 
-    comm = MPI.COMM_WORLD
-    rank = comm.Get_rank()
-    register(MPI.Finalize)
-
-    dev = drv.Device(rank)
-    ctx = dev.make_context()
-    # ctx.push()  # NOTE: `make_context` seems to already push to the context
-    # stack
-    register(ctx.pop)
-
-    nvtx.RangePop()
-    #---------------------------------------------------------------------------
-
+@nvtx.mark("Build CUDA code")
+def init():
 
     #___________________________________________________________________________
-    # Copy data to device
+    # Define worker function
     #
-    nvtx.RangePushA("Sending Data to Device")
+    mod = SourceModule("""
+    __global__
+    void multiply_cuda(double * dest, double * a, double * b, int N) {
+        const int index = threadIdx.x + blockIdx.x*blockDim.x;
+        for (int i=0; i<N+blockDim.x; i+=blockDim.x) {
+            if (index < N) {
+                dest[index] = a[index] * b[index];
+            }
+        }
+    }
+    """)
 
-    d_giant_data = to_gpu(giant_data)
-
-    nvtx.RangePop()
+    multiply_cuda = mod.get_function("multiply_cuda")
     #---------------------------------------------------------------------------
 
+    return multiply_cuda
+
+
+
+class Test(object):
+
+    def __init__(self, size, dim=1024):
+
+        #_______________________________________________________________________
+        # Generate sample data
+        #
+        nvtx.RangePushA("Generating Random Data")
+
+        self.N   = size
+        self.dim = dim
+        self.A   = np.random.rand(self.N)
+        self.B   = np.random.rand(self.N)
+
+        nvtx.RangePop()
+        #-----------------------------------------------------------------------
+
+
+        #_______________________________________________________________________
+        # Copy data to device
+        #
+        nvtx.RangePushA("Sending Data to Device")
+
+        self.d_A = to_gpu(self.A)
+        self.d_B = to_gpu(self.B)
+
+        nvtx.RangePop()
+        #-----------------------------------------------------------------------
+
+
+    def apply_fn(self, fn):
+
+        dest = np.zeros_like(self.A)
+        fn(drv.Out(dest), self.d_A, self.d_B, np.int64(self.N),
+           block=(self.dim, 1, 1), grid=(1, 1))
+
+        return dest
 
 
 if __name__ == "__main__":
 
-    test()
+    size = 2**10
+    if len(sys.argv) > 1:
+        size = int(sys.argv[1])
 
-    sleep(10)
+    print(f"Running test for {size} elements")
+
+    nvtx.mark_all_methods(Test)
+
+    fn   = init()
+    test = Test(size)
+
+    AB = test.apply_fn(fn)
